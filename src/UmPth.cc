7ec91e1d70fda1dc12ea27b1863be5535970d0c0
Jim Cadden <jmcadden@bu.edu>
2018-07-31 16:53:14 -0500

    Misc cleanup

--- src/UmPth.cc
+++ src/UmPth.cc
@@ -1,3 +1,8 @@
+//          Copyright Boston University SESA Group 2013 - 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #include "UmPth.h"
 
 // TODO: const
