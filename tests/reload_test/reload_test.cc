e894484ea2e77ca5ab5e47044f5d167e9d86d7de
Tommy Unger <tommyu@bu.edu>
2018-09-21 20:24:20 -0500

    Allow executing arbitrary js app.

--- tests/reload_test/reload_test.cc
+++ tests/reload_test/reload_test.cc
@@ -14,80 +14,44 @@
 #include "../../src/UmSV.h"
 #include "../../src/UmRegion.h"
 
-// NOTE: this fails at rump baremetal boostrap time.
-void loadFromElfTest(int numRuns){
+// const std::string my_cmd = R"({"cmdline":"bin/node-default /nodejsActionBase/tu_app.js"})";
+const std::string my_cmd = "";
 
-  for(int i = 0; i < numRuns; i++){
-    // Generate sv from the linked in Elf every round.
-    auto sv = umm::ElfLoader::createSVFromElf(&_sv_start);
-
-    // Create the Um Instance and set boot configuration
-    auto umi = std::make_unique<umm::UmInstance>(sv);
-
-    // Configure solo5 boot arguments
-    uint64_t argc = Solo5BootArguments(sv.GetRegionByName("usr").start, SOLO5_USR_REGION_SIZE);
-    umi->SetArguments(argc);
-
-    umm::manager->Load(std::move(umi));
-    umm::manager->runSV();
-    umm::manager->Unload();
-  }
-
-}
-
-void loadFromSVTest(int numRuns){
-  auto sv = umm::ElfLoader::createSVFromElf(&_sv_start);
-
-  for(int i = 0; i < numRuns; i++){
-    // Create the Um Instance and set boot configuration
-    auto umi = std::make_unique<umm::UmInstance>(sv);
-
-    // Configure solo5 boot arguments
-    uint64_t argc = Solo5BootArguments(sv.GetRegionByName("usr").start, SOLO5_USR_REGION_SIZE);
-    umi->SetArguments(argc);
-
-    umm::manager->Load(std::move(umi));
-    ebbrt::kprintf_force("Kicking off\n");
-    umm::manager->runSV();
-    umm::manager->Unload();
-
-  }
-}
-
-const umm::UmSV getSnap(){
-  auto sv = umm::ElfLoader::createSVFromElf(&_sv_start);
+const umm::UmSV* getSnap(){
+  // auto sv = umm::ElfLoader::createSVFromElf(&_sv_start);
 
   // Create the Um Instance and set boot configuration
-  ebbrt::kprintf_force(BLUE "Getting umi sv\n" RESET);
-  auto umi = std::make_unique<umm::UmInstance>(sv);
+  ebbrt::kprintf_force(CYAN "Getting umi sv\n" RESET);
+  auto umi = std::make_unique<umm::UmInstance>(umm::ElfLoader::createSVFromElf(&_sv_start));
+  ebbrt::kprintf_force(CYAN "Got umi sv\n" RESET);
 
   // Configure solo5 boot arguments
-  uint64_t argc = Solo5BootArguments(sv.GetRegionByName("usr").start, SOLO5_USR_REGION_SIZE);
+  uint64_t argc = Solo5BootArguments(umi->sv_.GetRegionByName("usr").start, SOLO5_USR_REGION_SIZE, my_cmd);
+
   umi->SetArguments(argc);
 
   // Get snap future.
   auto snap_f = umm::manager->SetCheckpoint(
-                                            // umm::ElfLoader::GetSymbolAddress("rumprun_main1"));
-                                            // umm::ElfLoader::GetSymbolAddress("solo5_app_main"));
-                                            umm::ElfLoader::GetSymbolAddress("uv_uptime"));
+                                            umm::ElfLoader::GetSymbolAddress("solo5_app_main"));
+  // umm::ElfLoader::GetSymbolAddress("rumprun_main1"));
+                                            // umm::ElfLoader::GetSymbolAddress("uv_uptime"));
 
   umm::manager->Load(std::move(umi));
   umm::manager->runSV();
   umm::manager->Unload();
 
   // assuming this doesn't change. add const.
-  ebbrt::kprintf_force("Grabbing snapshot from future\n");
   return snap_f.Get();
 }
 
 void loadFromSnapTest(int numRuns){
   auto snap = getSnap();
 
-  // NOTE: Starts at 1 unlike others.
-  for(int i = 1; i < numRuns; i++){
+  for(int i = 0; i < numRuns; i++){
     printf(RED "\n\n***** Deploying snapshot %d\n" RESET, i);
 
-    auto umi2 = std::make_unique<umm::UmInstance>(snap);
+    // Expect deep copy
+    auto umi2 = std::make_unique<umm::UmInstance>(*snap);
     umm::manager->Load(std::move(umi2));
     umm::manager->runSV();
     umm::manager->Unload();
@@ -97,8 +61,6 @@ void loadFromSnapTest(int numRuns){
 void AppMain() {
   // Initialize the UmManager
   umm::UmManager::Init();
-  // loadFromElfTest(1);
-  // loadFromSVTest(2);
-  loadFromSnapTest(1<<12);
+  loadFromSnapTest(1);
 
 }
