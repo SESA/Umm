25a7eea7cf739590eef21e1fcfac263a7af46a93
Tommy Unger <tommyu@bu.edu>
2019-03-29 14:36:02 -0500

    Reduce length of blocking timeout.

--- src/umm-solo5.cc
+++ src/umm-solo5.cc
@@ -18,7 +18,8 @@ void solo5_hypercall_poll(volatile void *arg) {
     return;
   }
   // TODO: Make this a block call on the active instance
-  umm::manager->Block(arg_->timeout_nsecs);
+  // umm::manager->Block(arg_->timeout_nsecs);
+  umm::manager->Block(1000);
   // return from block
 
   if (umm::manager->ActiveInstance()->HasData()) {
